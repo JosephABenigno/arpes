fbd4ba4b90e8fd4745a94b29b3e03e65598818ab
domna <florian.dobener@physik.hu-berlin.de>
2023-02-14 19:56:55 +0100

    Adds @Tommaso-Pincelli changes to nexus endstation

--- arpes/endstations/plugin/nexus.py
+++ arpes/endstations/plugin/nexus.py
@@ -1,35 +1,44 @@
 import xarray as xr
-import h5py as h5
+import h5py
+import numpy as np
 from arpes.endstations import SingleFileEndstation, add_endstation
 
-def load_nexus_file(path: str) -> xr.DataArray:
-    """Loads a MPES NeXus file and creates a DataArray from it.
-
-    Args:
-        path (str): The path of the .nxs file.
-
-    Returns:
-        xr.DataArray: The data read from the .nxs file.
-    """    
-    hf = h5.File(path, 'r')
-
-    return xr.DataArray(
-            hf['entry/data/Photoemission intensity'][:],
-            coords={
-                "BE": hf["entry/data/calculated_Energy"][:],
-                "kx": hf["entry/data/calculated_kx"][:],
-                "ky": hf["entry/data/calculated_ky"][:],
-            },
-            dims=["BE", "kx", "ky"],
-        )
 
 class NeXusEndstation(SingleFileEndstation):
+    """An endstation for reading arpes data from a nexus file."""
+
     PRINCIPAL_NAME = "nxs"
 
-    _TOLERATED_EXTENSIONS = {".nxs",}
+    _TOLERATED_EXTENSIONS = {
+        ".nxs",
+    }
 
-    def load_single_frame(self, frame_path: str = None, scan_desc: dict = None, **kwargs) -> xr.Dataset:
-        data = load_nexus_file(frame_path)
-        return xr.Dataset({'spectrum': data})
+    def load_nexus_file(self, path: str) -> xr.DataArray:
+        """Loads a MPES NeXus file and creates a DataArray from it.
+
+        Args:
+            path (str): The path of the .nxs file.
+
+        Returns:
+            xr.DataArray: The data read from the .nxs file.
+        """
+        with h5py.File(path, "r") as h5file:
+            return xr.DataArray(
+                h5file["entry/data/Photoemission intensity"][:],
+                coords={
+                    "tpp": np.squeeze(h5file["entry/data/calculated_Tpp"][:]),
+                    "BE": np.squeeze(np.transpose(h5file["entry/data/calculated_Energy"][:])),
+                    "kx": np.squeeze(h5file["entry/data/calculated_kx"][:]),
+                    "ky": np.squeeze(h5file["entry/data/calculated_ky"][:]),
+                },
+                dims=["delay", "eV", "kx", "ky"],
+            )
+
+    def load_single_frame(
+        self, frame_path: str = None, scan_desc: dict = None, **kwargs
+    ) -> xr.Dataset:
+        data = self.load_nexus_file(frame_path)
+        return xr.Dataset({"spectrum": data})
+
 
 add_endstation(NeXusEndstation)
